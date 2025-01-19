ccacbb38f581609b70a1d4ca7ae52912d9b8e6e5
Josue Arce <josuerc026@gmail.com>
2025-01-18 20:05:09 -0500

    install and init react router

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,16 @@ import './index.css'
 import "@department-of-veterans-affairs/component-library/dist/main.css";
 import App from './App.tsx'
 import { defineCustomElements } from '@department-of-veterans-affairs/web-components/loader'
+import { BrowserRouter, Route, Routes } from 'react-router';
 
 defineCustomElements();
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />} />
+            </Routes>
+        </BrowserRouter>
     </StrictMode>
 )
