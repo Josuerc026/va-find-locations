f7828ff4f929b815b12366a76a106827a1ff2675
Josue Arce <josuerc026@gmail.com>
2025-01-23 16:37:42 -0500

    chore: project structure updates

---
+++ src/components/Map.tsx
@@ -0,0 +1,60 @@
+import { Ref, useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { Facilities, Facility, Query } from '../interfaces';
+import { MAPBOX_TOKEN } from '../api';
+import { MB_STYLE } from '../constants';
+
+export default function Map({query, vaFacilities, setFocusedFacility}: {
+  query: Query;
+  vaFacilities: Facilities,
+  setFocusedFacility: (facility: Facility) => void
+}) {
+	const mapContainerRef: Ref<any> = useRef();
+	const mapRef: any = useRef();
+
+	useEffect(() => {
+		mapboxgl.accessToken = MAPBOX_TOKEN;
+
+		mapRef.current = new mapboxgl.Map({
+			style: MB_STYLE,
+			container: mapContainerRef.current,
+			center: [query.lng, query.lat],
+			zoom: 8
+		});
+
+		if (!vaFacilities.data.length) return;
+
+		const bounds = new mapboxgl.LngLatBounds();
+
+		vaFacilities?.data.forEach(({attributes}, index) => {
+			bounds.extend([attributes.long, attributes.lat]);
+
+			const elm = document.createElement('div');
+			elm.className = 'va-location-marker';
+			elm.textContent = `${++index}`;
+
+			const marker = new mapboxgl.Marker(elm)
+				.setLngLat([attributes.long, attributes.lat])
+				.addTo(mapRef.current);
+			
+			// set focused facility to scroll in results
+			marker.getElement().addEventListener('click', () => {
+				const coords = marker.getLngLat();
+				const focused = vaFacilities.data.find(({attributes}) => attributes.lat === coords.lat && attributes.long === coords.lng);
+				if (!focused) return;
+				setFocusedFacility(focused);
+			})
+		});
+
+		mapRef.current.fitBounds(bounds, {padding: 100});
+	}, [vaFacilities]);
+	
+	return (
+		<div
+			style={{ height: '100%', width: '100%' }}
+			ref={mapContainerRef}
+			className="map-container"
+		/>
+	);
+};
---
+++ src/components/search/Results.tsx
@@ -0,0 +1,144 @@
+import { VaPagination } from '@department-of-veterans-affairs/web-components/react-bindings';
+import { getAllQueryParams } from "../../utils";
+import { VaCard } from "@department-of-veterans-affairs/web-components/react-bindings";
+import { Facilities, Facility, Query } from "../../interfaces";
+import { VaLoadingIndicator } from '@department-of-veterans-affairs/web-components/react-bindings';
+import { VaButton } from '@department-of-veterans-affairs/web-components/react-bindings';
+import { Ref, useEffect, useRef } from 'react';
+
+export default function Results({vaFacilities, setQuery, query, focusedFacility}: {
+    vaFacilities: Facilities,
+    setQuery: (query: Query) => void,
+    query: Query,
+    focusedFacility: Facility
+}) {
+    const resultRef = useRef(null as Ref<HTMLDivElement>);
+    const handlePageSelect = (e: any) => {
+        const {name, lat, lng, serviceType, radius} = getAllQueryParams(window.location.search);
+        setQuery({
+            name,
+            lat,
+            lng,
+            serviceType,
+            radius,
+            page: e.detail.page
+        });
+    }
+
+    const onRetry = () => {
+        const {name, lat, lng, serviceType, radius, page} = getAllQueryParams(window.location.search);
+        setQuery({
+            name,
+            lat,
+            lng,
+            serviceType,
+            radius,
+            page
+        });
+    }
+
+    const scrollToResult = () => {
+        if (resultRef && resultRef?.current) {
+            resultRef.current.scrollIntoView(true, {behavior: 'smooth'});
+        }
+    }
+
+    useEffect(() => {
+        scrollToResult();
+    })
+
+    return (
+        <div className="p-3 results-wrapper d-flex flex-column">
+            <div className="row py-2">
+                <div className="col-md-8">
+                    {
+                        vaFacilities?.data?.length && vaFacilities?.meta?.pagination ? 
+                            (
+                                <h3 className="m-0 p-0">
+                                    Showing {vaFacilities?.meta?.pagination.perPage * (vaFacilities.meta.pagination.currentPage - 1)+1} - 
+                                    {(vaFacilities?.meta?.pagination.perPage * vaFacilities.meta.pagination.currentPage) - vaFacilities?.meta?.pagination.perPage * vaFacilities.meta.pagination.currentPage % vaFacilities?.meta?.pagination.totalEntries % vaFacilities.meta.pagination.perPage} of {vaFacilities?.meta?.pagination.totalEntries} Results
+                                </h3>
+                            ) : ''
+                    }
+                </div>
+                <div className="col-md-4 text-end">
+                    {/* TODO: Nav? */}
+                </div>
+            </div>
+
+            <div className={`results-container h-100 overflow-y-scroll flex-fill p-3 ${vaFacilities.loading || !vaFacilities?.data.length ? `d-flex flex-column justify-content-center` : ``}`}>
+                {
+                    vaFacilities.loading && !vaFacilities.error ? 
+                        <VaLoadingIndicator
+                            label="Loading"
+                            message="Loading facilities..."
+                        /> :
+                        <div className="row gx-2">
+                            {!!vaFacilities?.data?.length ? (
+                                vaFacilities.data.map((facility: any, index: number) => {
+                                    return (
+                                        <div 
+                                            ref={(el) => facility.id === focusedFacility.id && (resultRef.current = el)} 
+                                            key={facility.id}
+                                            id={facility.id === focusedFacility.id ? 'focused-facility' : ''} 
+                                            className="col-12 col-xl-6">
+                                                <div className="py-1 h-100">
+                                                    <VaCard class="p-3 h-100">
+                                                        <div className="p-0">
+                                                            <img 
+                                                                className="va-card__img"
+                                                                src={`https://s3-us-gov-west-1.amazonaws.com/content.www.va.gov/img/styles/3_2_medium_thumbnail/public/2021-08/${facility.attributes.name}.jpg`}
+                                                                onError={({currentTarget}) => {
+                                                                    currentTarget.onerror = null;
+                                                                    currentTarget.src = 'https://www.va.gov/HOMELESS/images/VA-seal-500x275.jpg';
+                                                                }}
+                                                            />
+                                                            <span className="facility-idx"><strong>{++index}</strong></span>
+                                                            <h3 className="mt-3 text-white"><a href={facility.attributes.website} target="_blank">{facility.attributes.name}</a></h3>
+                                                            <p>
+                                                                {[
+                                                                    facility.attributes.address.physical.city,
+                                                                    facility.attributes.address.physical.state,
+                                                                    facility.attributes.address.physical.zip
+                                                                ].join(', ')} 
+                                                            </p>
+                                                            <a className="text-white" href={`tel:${facility.attributes.phone.main}`}>{facility.attributes.phone.main}</a>
+                                                        </div>
+                                                    </VaCard>
+                                                </div>
+                                        </div>
+                                    )
+                                })
+                            ) : 
+                                <>
+                                    <h3>No results found for <u style={{textTransform: 'capitalize'}}>{query?.name}</u></h3>
+                                    <p>Try searching for VA locations in another location.</p>
+                                </>
+                            }
+                        </div>
+                }
+                {vaFacilities.error && !vaFacilities?.data?.length ? 
+                    (
+                        <>
+                            <p>There was an issue with the network request. Please try again.</p>
+                            <VaButton
+                                submit="prevent"
+                                onClick={onRetry}
+                                text="Retry Search"
+                            />
+                        </>
+                    )
+                    : ''
+                }
+            </div>
+            { vaFacilities?.data?.length && vaFacilities?.meta?.pagination? 
+                <VaPagination
+                    page={vaFacilities.meta.pagination.currentPage}
+                    pages={vaFacilities.meta.pagination.totalPages}
+                    max-page-list-length={vaFacilities.meta.pagination.perPage}
+                    onPageSelect={handlePageSelect}
+                /> : []
+            }
+        </div>
+    )
+}
